75c3aecac4e9d95fca30d1614ae691f5383d526c
habib <habib@SALAH>
2025-08-02 11:59:33 +0200

    Ajoutez des fichiers projet.

---
+++ EmployeeManager.Shared/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManager.Shared
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
