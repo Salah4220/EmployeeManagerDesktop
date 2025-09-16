15fa22633f8613464b78037c1e04caee74c57e66
Salah HABIBECHE <habibechesalaheddine1@gmail.com>
2025-09-16 20:54:38 +0200

    creation de la table task

--- EmployeeManager.Api/Migrations/20250916184240_AddTask.Designer.cs
+++ EmployeeManager.Api/Migrations/20250916184240_AddTask.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmployeeManager.Api;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EmployeeManager.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250916184240_AddTask")]
+    partial class AddTask
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmployeeManager.Shared/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Shared
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+        public int? UserId { get; set; }
+        public User User { get; set; }
+    }
+}
